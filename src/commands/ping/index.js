2cc03b8139546a755d3c021655f27e59971a0a14
PGM-vivo <acs109142@gm.ntcu.edu.tw>
2023-08-21 03:38:06 +0800

    EP5-vite-node, vue, pinia config and loader.js

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -1,20 +1,20 @@
 import {SlashCommandBuilder} from 'discord.js'
 
-// 一種導出方式
-// export const command = new SlashCommandBuilder()
-// .setName('ping')
-// .setDescription('ping command')
 
-// export const action = async (ctx) => {
-//     await ctx.reply('pong')
-// }
+export const command = new SlashCommandBuilder()
+.setName('ping')
+.setDescription('ping command')
 
-export default {
-    command: new SlashCommandBuilder()
-    .setName('Ping')
-    .setDescription('ping command'),
-    
-    action: async (ctx) => {
-        ctx.reply('pong!')
-    },
+export const action = async (ctx) => {
+    await ctx.reply('pong')
 }
+
+// export default {
+//     command: new SlashCommandBuilder()
+//     .setName('Ping')
+//     .setDescription('ping command'),
+    
+//     action: async (ctx) => {
+//         ctx.reply('pong!')
+//     },
+// }
